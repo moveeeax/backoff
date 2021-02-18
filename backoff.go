c847a761f68b519365fc187ee19134452c686832
Michael Tarassov <michael@tarassov.me>
2021-02-18 17:57:47 +0300

    Add Backoff struct with Default constructor and Reset

---
+++ backoff.go
@@ -0,0 +1,83 @@
+package backoff
+
+import (
+	"time"
+)
+
+// Stop is returned by NextBackOff when no further retries should be attempted.
+const Stop time.Duration = -1
+
+// Backoff computes exponential backoff intervals with optional jitter.
+type Backoff struct {
+	// InitialInterval is the first delay duration.
+	InitialInterval time.Duration
+	// MaxInterval caps the computed delay before jitter is applied.
+	MaxInterval time.Duration
+	// MaxElapsed is the total time after which NextBackOff returns Stop.
+	// Zero means no limit.
+	MaxElapsed time.Duration
+	// Multiplier is the growth factor applied each call (e.g. 1.5).
+	Multiplier float64
+	// RandomizationFactor adds ±jitter in the range [0, 1).
+	RandomizationFactor float64
+
+	currentInterval time.Duration
+	startTime       time.Time
+}
+
+// Default returns a Backoff with sensible production defaults:
+//   - InitialInterval: 500ms
+//   - MaxInterval:     60s
+//   - MaxElapsed:      15min
+//   - Multiplier:      1.5
+//   - RandomizationFactor: 0.5
+func Default() *Backoff {
+	b := &Backoff{
+		InitialInterval:     500 * time.Millisecond,
+		MaxInterval:         60 * time.Second,
+		MaxElapsed:          15 * time.Minute,
+		Multiplier:          1.5,
+		RandomizationFactor: 0.5,
+	}
+	b.Reset()
+	return b
+}
+
+// Reset restores the backoff to its initial state and records the current time
+// as the start of the retry window.
+func (b *Backoff) Reset() {
+	b.currentInterval = b.InitialInterval
+	b.startTime = time.Now()
+}
+
+// NextBackOff returns the next interval to wait before retrying.
+// It returns Stop when MaxElapsed has been exceeded (if MaxElapsed > 0).
+func (b *Backoff) NextBackOff() time.Duration {
+	// Check elapsed time.
+	if b.MaxElapsed > 0 {
+		elapsed := time.Since(b.startTime)
+		if elapsed >= b.MaxElapsed {
+			return Stop
+		}
+	}
+
+	interval := b.currentInterval
+
+	// Advance current interval for next call, capping at MaxInterval.
+	b.advanceInterval()
+
+	return interval
+}
+
+// advanceInterval multiplies currentInterval by Multiplier and caps at MaxInterval.
+func (b *Backoff) advanceInterval() {
+	if b.Multiplier <= 0 {
+		return
+	}
+	next := float64(b.currentInterval) * b.Multiplier
+	if b.MaxInterval > 0 && next > float64(b.MaxInterval) {
+		b.currentInterval = b.MaxInterval
+	} else {
+		b.currentInterval = time.Duration(next)
+	}
+}
